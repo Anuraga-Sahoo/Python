b7a80ee2a53b018d34b694c29765d9e4502735bc
Anuraga Sahoo <anuragliku.2020@gmail.com>
2024-06-20 21:40:26 +0530

    Variables

---
+++ 01 Variables and Data type/01Intro.py
@@ -0,0 +1,19 @@
+# What is python
+# Python is simple and easy
+# free and open source
+# High leavel language
+# developed by guido van rossum
+# It is portable
+
+print("Hello world")
+
+# Variable in python
+
+name = "Anurag"
+age = 23
+price = 25.99
+print(name)
+
+# rules for identifier
+# 1- identifier can not start with digit
+# 2 - we cant use special symbols like !,@,#,%,$ in  identifier 
---
+++ String/Slicing.py
@@ -0,0 +1,20 @@
+# Slicing in String
+
+b = "hello world"
+slicing = b[2 : 5]
+print(slicing)
+
+# slice from the start
+slicing = b [ : 5]
+print(slicing)
+
+#slice from the end
+slicing = b [2:]
+print(slicing)
+
+# negative indexing
+# Use negative indexes to start the slice from the end of the string:
+
+slicing = b[-5: -2]
+print(slicing)
+
